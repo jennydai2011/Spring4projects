a0f2432383dca3f264472d04f208ba4656e23c6f
jennydai2011 <jennydai2011@gmail.com>
2019-02-16 09:19:11 -0700

    spring5reactdemo

---
+++ spring5reactdemo/src/main/java/com/jdai/spring5reactdemo/Spring5reactdemoApplication.java
@@ -0,0 +1,14 @@
+package com.jdai.spring5reactdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5reactdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5reactdemoApplication.class, args);
+	}
+
+}
+
