079d802d3e7d45443936c2b391d2d09b3e086460
jennydai2011 <jennydai2011@gmail.com>
2020-05-31 17:35:09 -0600

    accessing data jpa through restful front end which skip the mvc layer

---
+++ spring-jpa-data-rest/src/main/java/com/jdai/springjpadatarest/SpringJpaDataRestApplication.java
@@ -0,0 +1,13 @@
+package com.jdai.springjpadatarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaDataRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaDataRestApplication.class, args);
+	}
+
+}
